bf1973bf6db51b2b26c9ea8a3aaa304253f93b29
Randy Mai <randymai@yahoo.com>
2018-01-25 16:55:36 -0600

    fix js so that confirmation will show

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,8 +1,8 @@
-$('contact-form').on('submit',
-	function() {
-		console.log('The form was submitted.');
-		var email = $('email').val();
-		$(this).remove();
-		$('body').append('<h1>Thank You. Email Address: ' + email + '<h/1>');
-	}
+$('#contact-form').on('submit',
+  function() {
+    console.log('The form was submitted.');
+    var email = $('#email').val();
+    $(this).remove();
+    $('body').append('<h1>Thank You. Email Address: ' + email + '</h1>');
+  }
 );
